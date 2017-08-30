eae84c4e708c162b3fa8c6932a58edea5d36cc89
Beat Scholl <beat.scholl@itsystems.ch>
2017-08-30 10:18:48 +0200

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,22 @@ import './App.css';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
+// tslint:disable-next-line:no-any
+class App extends React.Component<any, any> {
+  // tslint:disable-next-line:no-any
+  constructor(props: any) {
+    super(props);
+    this.state = {value: ''};
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // tslint:disable-next-line:no-any
+  handleSubmit(event: any) {
+    alert('submit button clicked');
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,6 +29,12 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            call logic app
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
